f78c76b9c067ea22267842a879bcb34ba9801871
Blen Agaze <ban3291@rit.edu>
2025-03-17 10:36:21 +0400

    PastMapping is added in the controller

--- src/main/java/edu/rit/week_8_again/CONTROLLER/appController.java
+++ src/main/java/edu/rit/week_8_again/CONTROLLER/appController.java
@@ -4,6 +4,7 @@ package edu.rit.week_8_again.CONTROLLER;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class appController {
@@ -12,6 +13,17 @@ public class appController {
     public String getStudent(Model m){
         var s=ss.findStudents();
         m.addAttribute("students",s);
+        return "students";
+    }
+
+
+
+    @PostMapping("/students")
+
+    public String postStudents(Student s,Model m){
+        ss.addStudents(s);
+        var students=ss.findStudents();
+        m.addAttribute("students",students);
         return "students";
     }
 }
