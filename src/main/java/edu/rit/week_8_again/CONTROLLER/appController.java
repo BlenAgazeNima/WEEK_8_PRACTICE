97051ec9249ed1957541028a8854db5f5e9afab2
Blen Agaze <ban3291@rit.edu>
2025-03-17 10:26:11 +0400

    Initial configuration for controller class

--- src/main/java/edu/rit/week_8_again/CONTROLLER/appController.java
+++ src/main/java/edu/rit/week_8_again/CONTROLLER/appController.java
@@ -1,4 +1,17 @@
 package edu.rit.week_8_again.CONTROLLER;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class appController {
+    private StudentService ss;
+    @GetMapping("/students")
+    public String getStudent(Model m){
+        var s=ss.findStudents();
+        m.addAttribute("students",s);
+        return "students";
+    }
 }
