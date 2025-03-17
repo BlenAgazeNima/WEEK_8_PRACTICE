dfd696248f6191e2069d92ba2785773a83c8393f
Blen Agaze <ban3291@rit.edu>
2025-03-17 11:18:10 +0400

    Minor changes in some of the files

--- src/main/java/edu/rit/week_8_again/CONTROLLER/appController.java
+++ src/main/java/edu/rit/week_8_again/CONTROLLER/appController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class appController {
     private StudentService ss;
+    public appController(StudentService ss){
+        this.ss=ss;
+    }
     @GetMapping("/students")
     public String getStudent(Model m){
         var s=ss.findStudents();
